16ad3783320e34f92d32a1e4c4897dffc76d20cd
Delfino Farfan <delfino.farfan18@gmail.com>
2022-04-16 18:18:50 -0500

    Week 2 HW

---
+++ week2hw/week2hw.js
@@ -0,0 +1,13 @@
+let pizzaPlace = "Minskys";
+let numberOfToppings = 6;
+
+console.log(`"Here at ${pizzaPlace} we now offer ${numberOfToppings} toppings on our latest creation!"`);
+
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+if(numberOfToppings < 10) {
+    "Quality, not quantity."
+} else {
+    "A whole lot of pizza."
+}
