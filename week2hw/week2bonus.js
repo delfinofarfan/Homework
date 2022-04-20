56208f339a677584bc0a8ca2176385e2964b3f1e
Delfino Farfan <delfino.farfan18@gmail.com>
2022-04-20 17:52:52 -0500

    corrections

---
+++ week2hw/week2bonus.js
@@ -0,0 +1,23 @@
+// let pizzaPlace = "Minskys";
+let numberOfToppings = 32;
+
+// console.log(`"Here at ${pizzaPlace} we now offer ${numberOfToppings} toppings on our latest creation!"`);
+
+// console.log(typeof pizzaPlace);
+// console.log(typeof numberOfToppings);
+
+// if(numberOfToppings < 10) {
+//     "Quality, not quantity."
+// } else {
+//     "A whole lot of pizza."
+// }
+// while (numberOfToppings <= 10) {
+
+// console.log(numberOfToppings);
+
+// numberOfToppings++;
+// }
+
+for (let i = 2; i <= numberOfToppings; i += 2) {
+  console.log(i);
+}
--- week2hw/week2hw.js
+++ week2hw/week2hw.js
@@ -1,13 +1,15 @@
 let pizzaPlace = "Minskys";
 let numberOfToppings = 6;
 
-console.log(`"Here at ${pizzaPlace} we now offer ${numberOfToppings} toppings on our latest creation!"`);
+console.log(
+  `Here at ${pizzaPlace} we now offer ${numberOfToppings} toppings on our latest creation!`
+);
 
-console.log(typeof pizzaPlace);
-console.log(typeof numberOfToppings);
+console.log(pizzaPlace, typeof pizzaPlace);
+console.log(numberOfToppings, typeof numberOfToppings);
 
-if(numberOfToppings < 10) {
-    "Quality, not quantity."
+if (numberOfToppings < 10) {
+  ("Quality, not quantity.");
 } else {
-    "A whole lot of pizza."
+  ("A whole lot of pizza.");
 }
